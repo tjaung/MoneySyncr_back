e3c4c660409c8eec32d1db600009b067c516c5d8
Tim Jaung <timjaung@Tims-MacBook-Pro.local>
2024-10-19 17:16:13 -0400

    implement original models.py

--- users/models.py
+++ users/models.py
@@ -5,20 +5,17 @@ from django.contrib.auth.models import (
     PermissionsMixin
 )
 
+
 class UserAccountManager(BaseUserManager):
-    def create_user(self, username, email=None, password=None, **kwargs):
-        if not username:
-            raise ValueError('The Username field is required')
-
+    def create_user(self, email, password=None, **kwargs):
         if not email:
-            raise ValueError('The Email field is required')
+            raise ValueError('Users must have an email address')
 
-        # Normalize email and create user model
         email = self.normalize_email(email)
+        email = email.lower()
 
         user = self.model(
             email=email,
-            # username=username,  # Explicitly pass username
             **kwargs
         )
 
@@ -27,26 +24,25 @@ class UserAccountManager(BaseUserManager):
 
         return user
 
-    def create_superuser(self, username, email=None, password=None, **kwargs):
-        kwargs.setdefault('is_staff', True)
-        kwargs.setdefault('is_superuser', True)
+    def create_superuser(self, email, password=None, **kwargs):
+        user = self.create_user(
+            email,
+            password=password,
+            **kwargs
+        )
 
-        return self.create_user(username, email, password, **kwargs)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
 
 
 class UserAccount(AbstractBaseUser, PermissionsMixin):
-    firstName = models.CharField(max_length=255)
-    lastName = models.CharField(max_length=255)
-
-    address = models.CharField(max_length=50, blank=True, null=True)  # Optional
-    city = models.CharField(max_length=50, blank=True, null=True)  # Optional
-    state = models.CharField(max_length=50, blank=True, null=True)  # Optional
-    zipCode = models.CharField(max_length=15, blank=True, null=True)  # Optional
-    phone = models.CharField(max_length=15, blank=True, null=True)  # Optional
-
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True, max_length=255)
-    username = models.CharField(max_length=50, unique=True)
-    password = models.CharField(max_length=50)
+
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
@@ -54,7 +50,7 @@ class UserAccount(AbstractBaseUser, PermissionsMixin):
     objects = UserAccountManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'firstName', 'lastName', 'password']
+    REQUIRED_FIELDS = ['first_name', 'last_name']
 
     def __str__(self):
         return self.email
